ca2a076734a7222e84bfc5e12f8097e3b8920888
shun.tomoyose <scarlet.of.the.incas@gmail.com>
2020-03-06 16:13:39 +0900

    FAQの導入テスト中、問い合わせ先のUI追加

--- default.js
+++ default.js
@@ -1,47 +1,75 @@
-const app = new Vue({
-  el: '#app',
-  vuetify: new Vuetify({
-    theme: {
-      themes: {
-        light: {
-          primary: '#03A9F4',
-          // brand: '#03A9F4',
-          unanswered: '#999999',
-          correct: '#0288d1',
-          wrong: '#e57373'
-        }
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#03A9F4',
+        // brand: '#03A9F4',
+        unanswered: '#999999',
+        correct: '#0288d1',
+        wrong: '#e57373'
       }
     }
-  }),
+  }
+})
+
+const Search = { template: '#search' }
+const Contact = { template: '#contact' }
+
+const store = new Vuex.Store({
+  state: {
+    questions: []
+  },
+  mutations: {
+    setQuestions(state, arr) {
+      // console.log(arr)
+      state.questions = arr
+    }
+  },
+  actions: {
+    getQuestions({commit}) {
+      axios.get('./questions.json').then(res => {
+        // console.log(res.data)
+        commit('setQuestions', res.data)
+      })
+    }
+  },
+  // getters: {}
+})
+
+
+const app = new Vue({
+  el: '#app',
+  vuetify,
+  store,
   computed: {
     keywordSplit() {
       const keywordReplaced = this.input.keyword.replace(/　+/g, ' ')
       return keywordReplaced.split(' ').filter(v => v)
+    },
+    filteredQuestions() {
+      console.log(this.$store.state.questions)
+      const filtered = this.$store.state.questions.filter(q => {
+        return true
+      })
+      return filtered
     }
   },
   methods: {},
-  data: {
-    drawer: {
-      show: false,
-      selected: ''
-    },
-    input: {
-      keyword: 'test'
+  data() {
+    return {
+      drawer: {
+        show: false,
+        selected: ''
+      },
+      input: {
+        keyword: 'test'
+      },
+      panel: [0]
     }
+  },
+  mounted() {
+    console.log('mounted')
+    this.$store.dispatch('getQuestions')
   }
 })
-
-
-
-const store = new Vuex.Store({
-  state: {},
-  mutations: {
-
-  }
-})
-
-const router = new VueRouter({
-  routes: [
-    {path: '/', component: Home}
-  ]
-})
+// }).$mount('#app')
